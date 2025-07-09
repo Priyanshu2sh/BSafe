adfb433a823ac58ca81467abc5c192df68cb2ab1
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-07-09 14:15:49 +0530

    Remove unused include import from urls.py and comment out myapp.urls path

--- bsafe/urls.py
+++ bsafe/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/upload-image", views.ImageUploadAPI.as_view(), name="upload-image"),
-    path('', include('myapp.urls')),
+    # path('', include('myapp.urls')),
 ]
